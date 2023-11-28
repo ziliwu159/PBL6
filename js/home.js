ad04169f9732eca365181b193fdf65c7cd11a6f7
Yulia Pipka <yuliaziliwu59@sma.belajar.id>
2023-11-28 13:47:13 +0700

    kategori blm siap

---
+++ js/home.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-btn").addEventListener("click", function () {
+  document.querySelector(".kategori").classList.toggle("active");
+  document.querySelector(".bx-menu").classList.toggle("bx-x");
+});
